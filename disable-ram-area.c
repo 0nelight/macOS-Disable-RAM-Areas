467bd33346ab2253c8717889d9a101ca65b177b8
0nelight <vsvismeista@gmail.com>
2024-09-10 14:02:32 +0200

    funktioniert!

--- disable-ram-area.c
+++ disable-ram-area.c
@@ -6,24 +6,55 @@
 #include <efiapi.h>
 #include <efierr.h>
 
+
 EFI_STATUS
-efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE * systemTable) {
-  SIMPLE_TEXT_OUTPUT_INTERFACE * conOut = systemTable -> ConOut;
+efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systemTable) {
+    SIMPLE_TEXT_OUTPUT_INTERFACE *conOut = systemTable->ConOut;
 
-  conOut -> OutputString(conOut, L"Set Bad Ram...\r\n");
+    conOut->OutputString(conOut, L"Disable Ram Area...\r\n");
 
-  EFI_PHYSICAL_ADDRESS Addr = 12884901888;
+    EFI_PHYSICAL_ADDRESS lowest_error_address = 0x300000000;
+    EFI_PHYSICAL_ADDRESS highest_error_address = 0x309FFFF48;
 
-  EFI_BOOT_SERVICES * gBS;
-  gBS = systemTable -> BootServices;
+    EFI_BOOT_SERVICES *gBS;
+    gBS = systemTable->BootServices;
 
-  gBS -> AllocatePages(2, 8, 40960, & Addr);
-  
-  gBS->Stall(1000000);
+    UINTN num_pages = (highest_error_address - lowest_error_address + 4095) / 4096;
 
-  conOut -> OutputString(conOut, L"ok!\r\n");
-  
-  gBS->Stall(1000000);
+    EFI_STATUS status = gBS->AllocatePages(AllocateAddress, EfiRuntimeServicesData, num_pages, &lowest_error_address);
+    if (EFI_ERROR(status)) {
+        conOut->OutputString(conOut, L"Failed to allocate pages!\r\n");
+        return status;
+    }
 
-  return EFI_SUCCESS;
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+    conOut->OutputString(conOut, L"Disabled Ram Area!\r\n");
+
+    gBS->Stall(3000000); // Stall for 3 seconds
+
+    return EFI_SUCCESS;
 }
