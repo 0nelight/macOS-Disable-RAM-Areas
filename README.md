a8e7847349cda3b6938d5364440fba592f2d845c
Brian C. Arnold <connect@brian-arnold.dev>
2024-10-22 19:43:20 -0400

    Changed to struct with moveNext behavior, so I can allocate a chain of ranges to be allocated out of the OS's usable address space.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# EFI Memory Patch Utility for macOS 
+# EFI Memory Patch Utility for x86_64 systems
 
-**Currently the activation of this Utility needs a bit of effort. But if you put some effort into reading and understanding all the information, a lot of people can manage to get it working.** 
+(Note: While much of this documentation refers to MacOS and Apple Hardware, since this all relies on the UEFI standard, it should work on all x86_64 EFI loaded operating systems on any compatible hardware.)
+
+**Currently the activation of this Utility needs a bit of effort. But if you put some effort into reading and understanding all the information, a lot of people can manage to get it working.**
 
 Are you experiencing system hangs or kernel panics on your Apple Mac or MacBook due to faulty memory? This EFI utility allows you to **disable defective memory regions** before macOS boots, preventing system crashes or unbootable Systems.
 
@@ -10,7 +12,8 @@ If you're receiving errors like:
 
 This utility offers a software solution by marking specific memory areas as defective, so macOS avoids them during runtime.
 
-### Steps:
+### Steps
+
 1. **Inform yourself**: Read through this README, [the Release-Notes](https://github.com/0nelight/macOS-Disable-RAM-Areas/releases/tag/v0.2.1-beta) and the [Discussions-Page](https://github.com/0nelight/macOS-Disable-RAM-Areas/discussions)
 2. **Identify Faulty Memory**: Use diagnostic tools like [Memtest86+](https://www.memtest.org/) in Error-Summary-Mode to get the lowest and highest address of misbehaiving RAM.
 3. **Run this EFI-Utility**: Use tools like [rEFInd](https://www.rodsbooks.com/refind/) to run this EFI-utility once before booting into macOS. This will mark the faulty RAM Area as unavailable for MacOS and therefore allowing a normal Operation of MacOs with only having a little bit less RAM.
@@ -18,46 +21,54 @@ This utility offers a software solution by marking specific memory areas as defe
 5. **Automate Booting into MacOs with disabled RAM-Areas**: See [this Guide](https://github.com/0nelight/macOS-Disable-RAM-Areas/discussions/3).
 4. **In case you have Questions not answered already - please open [a new Discussion](https://github.com/0nelight/macOS-Disable-RAM-Areas/discussions/new?category=q-a).**
 
+## Disclaimer
 
-## Disclaimer:
 This utility is not affiliated with, authorized, endorsed by, or in any way officially connected to Apple Inc. The use of this software may affect the operation of your system and is provided as-is, without any express or implied warranties. It is intended for personal use only and serves as a workaround for users experiencing issues with defective memory on Apple Mac or MacBook devices. By using this utility, you acknowledge that Apple Inc. bears no responsibility for any consequences arising from its use. Do not attribute any success or lack of success with this tool to the Apple brand.
 
 The author of this repository assumes no responsibility for any time invested in installing this tool or for any outcomes, whether expected or unexpected. This is a personal, hobbyist project with no formal support or obligations. Use at your own risk.
 
 ## Important Note: Compatibility
+
 This utility has been successfully in use for several years on an **MacBook Pro (Late 2013)** running **macOS Big Sur**. Since uploading this EFI-Utility on Github it seems other people got it working too. For example on [Macbook Pro 2019 16'](https://github.com/0nelight/macOS-Disable-RAM-Areas/discussions/3#discussioncomment-10753593). Please read the other Success-Stories on the [Issues-Page](https://github.com/0nelight/macOS-Disable-RAM-Areas/issues?q=label%3Asuccess+is%3Aclosed) or consider [writing one yourself](https://github.com/0nelight/macOS-Disable-RAM-Areas/issues/new?assignees=&labels=success&projects=&template=success-story.md&title=Success-Story) (please fill in your System Information) in case you found this Utility helpful.
 
-## Motivation:
-I see this as a great way to extend hardware lifespan, protect customer and shareholder value, and fight inflation by lowering demand for new devices, thus preserving the purchasing power of the dollar through more efficient resource use. 
+## Motivation
+
+I see this as a great way to extend hardware lifespan, protect customer and shareholder value, and fight inflation by lowering demand for new devices, thus preserving the purchasing power of the dollar through more efficient resource use.
 Also such tools help keeping your soil and rivers less polluted from E-Waste and manufacturing waste material, which is good for your health and your health insurance.
 
-
 ## Why This Utility for Apple Mac and MacBook Users?
+
 Many modern Apple devices, including Macs and MacBooks, come with memory chips soldered directly onto the motherboard. Replacing or reflowing these chips requires advanced tools and expertise, making hardware repair difficult and expensive.
 
 This utility provides a **software-based workaround** by disabling defective memory blocks before macOS boots. It’s similar to the GNU GRUB `badram` command, but tailored for Apple hardware and macOS environments.
 
-## Benefits:
+## Benefits
+
 - **No Costly Hardware Repair**: Solve memory-related issues without the need for expensive hardware repairs or professional services.
 - **Extend the Life of Your Mac/MacBook**: Keep your Apple device running smoothly, even with defective memory regions.
 - **Reduces E-Waste**: This tool extents the life of your Apple Computer and therefore reduces E-Waste.
+
 ---
 
 ToDos (Contributions welcome!):
- - Testing with newer devices and newer macOs Versions
- - Provide better documentation (how to compile, "apt-get install gnu-efi")
 
-## Some additonal Notes:
+- Testing with newer devices and newer macOs Versions
+- Provide better documentation (how to compile, "apt-get install gnu-efi")
 
-### Screenshot-from refind (instead of grubx64.efi you can have your DISABLE-RAM.EFI utility there):
+## Some additonal Notes
+
+### Screenshot-from refind (instead of grubx64.efi you can have your DISABLE-RAM.EFI utility there)
+
 ![refind](https://github.com/user-attachments/assets/86a70d90-0975-4411-bf5b-49966757245d)
 
-### The Place of this Utility within the Boot Process of a Mac:
+### The Place of this Utility within the Boot Process of a Mac
+
 ![grafik](https://github.com/user-attachments/assets/2ee9237c-1e67-4339-aa63-3c1d718bb8ba)
 
-### Documentation of AllocatePages-Function:
+### Documentation of AllocatePages-Function
+
 Documentation of this function: `gBS -> AllocatePages(2, 8, 40960, & Addr);` [efi-v1-10-specification.pdf](https://github.com/user-attachments/files/16910413/efi-v1-10-specification.pdf) Page: 119
 
-### More Information on Mac-Internals:
-https://eclecticlight.co/mac-troubleshooting-summary/
+### More Information on Mac-Internals
 
+<https://eclecticlight.co/mac-troubleshooting-summary/>
--- disable-ram-area.c
+++ disable-ram-area.c
@@ -1,112 +1,176 @@
 #include <efi.h>
 #include <efilib.h>
 
+struct ADDRESS_RANGE
+{
+  EFI_PHYSICAL_ADDRESS startAddress;
+  UINT64 pageCount;
+  struct ADDRESS_RANGE *next;
+};
 
-EFI_STATUS ParseCommandLine(CHAR16 *command_line, EFI_PHYSICAL_ADDRESS *Addr, EFI_PHYSICAL_ADDRESS *EndAddr, UINTN *Stalltime) {
-    // Ensure command_line is not null
-    if (command_line == NULL) {
-        Print(L"No command line arguments provided.\n");
-        return EFI_INVALID_PARAMETER;
-    }
+EFI_FILE_HANDLE GetVolume(EFI_HANDLE image)
+{
+  EFI_LOADED_IMAGE *loaded_image = NULL;                  /* image interface */
+  EFI_GUID lipGuid = EFI_LOADED_IMAGE_PROTOCOL_GUID;      /* image interface GUID */
+  EFI_FILE_IO_INTERFACE *IOVolume;                        /* file system interface */
+  EFI_GUID fsGuid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID; /* file system interface GUID */
+  EFI_FILE_HANDLE volumeHandle;                                 /* the volume's interface */
 
-    Print(L"Command line: %s\n", command_line);
+  /* get the loaded image protocol interface for our "image" */
+  uefi_call_wrapper(BS->HandleProtocol, 3, image, &lipGuid, (void **) &loaded_image);
+  /* get the volume handle */
+  uefi_call_wrapper(BS->HandleProtocol, 3, loaded_image->DeviceHandle, &fsGuid, (VOID*)&IOVolume);
+  uefi_call_wrapper(IOVolume->OpenVolume, 2, IOVolume, &volumeHandle);
+  return volumeHandle;
+}
 
-    // Count the number of arguments
-    int arg_count = 0;
-    CHAR16 *ptr = command_line;
-    while (*ptr) {
-        while (*ptr == ' ') ptr++;  // Skip spaces
-        if (*ptr) arg_count++;      // Found an argument
-        while (*ptr && *ptr != ' ') ptr++;  // Move to next space or end
-    }
+UINT64 GetFileSize(EFI_FILE_HANDLE FileHandle)
+{
+  UINT64 ret;
+  EFI_FILE_INFO       *FileInfo;         /* file information structure */
+  /* get the file's size */
+  FileInfo = LibFileInfo(FileHandle);
+  ret = FileInfo->FileSize;
+  FreePool(FileInfo);
+  return ret;
+}
 
-    // If four arguments are provided, remove the first one
-    if (arg_count == 4) {
-        while (*command_line && *command_line != ' ') command_line++;  // Skip first argument
-        while (*command_line == ' ') command_line++;  // Skip spaces after the first argument
-    }
+EFI_FILE_HANDLE GetBadRamFileHandle(EFI_FILE_HANDLE volumeHandle, CHAR16 *fileName, EFI_FILE_HANDLE fileHandle)
+{
+  uefi_call_wrapper(volumeHandle->Open, 5, volumeHandle, &fileHandle, fileName, EFI_FILE_MODE_READ, EFI_FILE_READ_ONLY | EFI_FILE_HIDDEN | EFI_FILE_SYSTEM);
+  return fileHandle;
+}
 
-    // Parse the first address (Hex)
-    *Addr = 0;
-    while (*command_line && *command_line != ' ') {
-        if (*command_line >= '0' && *command_line <= '9') {
-            *Addr = (*Addr << 4) | (*command_line - '0');
-        } else if (*command_line >= 'a' && *command_line <= 'f') {
-            *Addr = (*Addr << 4) | (*command_line - 'a' + 10);
-        } else if (*command_line >= 'A' && *command_line <= 'F') {
-            *Addr = (*Addr << 4) | (*command_line - 'A' + 10);
-        }
-        command_line++;
-    }
-
-    while (*command_line == ' ') command_line++;  // Skip spaces
-
-    // Parse the second address (Hex)
-    *EndAddr = 0;
-    while (*command_line && *command_line != ' ') {
-        if (*command_line >= '0' && *command_line <= '9') {
-            *EndAddr = (*EndAddr << 4) | (*command_line - '0');
-        } else if (*command_line >= 'a' && *command_line <= 'f') {
-            *EndAddr = (*EndAddr << 4) | (*command_line - 'a' + 10);
-        } else if (*command_line >= 'A' && *command_line <= 'F') {
-            *EndAddr = (*EndAddr << 4) | (*command_line - 'A' + 10);
-        }
-        command_line++;
-    }
-
-    while (*command_line == ' ') command_line++;  // Skip spaces
-
-    // Parse the stall time (Decimal)
-    *Stalltime = 0;
-    while (*command_line && *command_line >= '0' && *command_line <= '9') {
-        *Stalltime = (*Stalltime * 10) + (*command_line - '0');
-        command_line++;
-    }
-
-    return EFI_SUCCESS;
+UINT8* ReadBadRamFile(EFI_FILE_HANDLE fileHandle)
+{
+  UINT64 fileSize = GetFileSize(fileHandle);
+  UINT8* fileBuffer = AllocatePool(fileSize);
+  uefi_call_wrapper(fileHandle->Read, 3, fileHandle, &fileSize, fileBuffer);
+  return fileBuffer;
 }
 
 
-EFI_STATUS
-efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    EFI_STATUS Status;
-	EFI_LOADED_IMAGE *loaded_image;
-    EFI_PHYSICAL_ADDRESS Addr, EndAddr;
-    UINTN Stalltime;
+struct ADDRESS_RANGE* ParseCommandLine(CHAR16 *command_line, UINTN *Stalltime) {
+  // Ensure command_line is not null
+  if (command_line == NULL) {
+      Print(L"No command line arguments provided.\n");
+      return 0x0;
+  }
+  struct ADDRESS_RANGE *initialAddressRange = 0x0;
+  UINT64 StartAddr = 0x0;
+  UINT64 EndAddr = 0x0;
 
-   Status = uefi_call_wrapper(BS->HandleProtocol, 3, ImageHandle, &LoadedImageProtocol, (void **)&loaded_image);
-    if (EFI_ERROR(Status)) {
-       Print(L"Error HandleProtocol: %r\n", Status);
-       uefi_call_wrapper(BS->Stall, 1, 5000000);
-       return Status;
-    }
 
-    CHAR16 *command_line = (CHAR16 *)loaded_image->LoadOptions;
-    if (command_line == NULL) {
-        Print(L"No command line arguments provided.\n");
-        return EFI_INVALID_PARAMETER;
-    }
+  Print(L"Command line: %s\n", command_line);
+
+  // Count the number of arguments
+  int arg_count = 0;
+  CHAR16 *ptr = command_line;
+  while (*ptr) {
+      while (*ptr == ' ') ptr++;  // Skip spaces
+      if (*ptr) arg_count++;      // Found an argument
+      while (*ptr && *ptr != ' ') ptr++;  // Move to next space or end
+  }
+  
+  switch (arg_count)
+  { 
+    // If 4 arguments are provided, we are probably getting
+    // the name of the EFI executable as the first one. We
+    // can just skip that.
+    case 4:
+      while (*command_line && *command_line != ' ') command_line++;  // Skip first argument
+      while (*command_line == ' ') command_line++;  // Skip spaces after the first argument
+      // Fall through, we can keep parsing the command line.
+    case 3:
+      // Parse the first address (Hex)
+      StartAddr = 0;
+      while (*command_line && *command_line != ' ') {
+          if (*command_line >= '0' && *command_line <= '9') {
+              StartAddr = (StartAddr << 4) | (*command_line - '0');
+          } else if (*command_line >= 'a' && *command_line <= 'f') {
+              StartAddr = (StartAddr << 4) | (*command_line - 'a' + 10);
+          } else if (*command_line >= 'A' && *command_line <= 'F') {
+              StartAddr = (StartAddr << 4) | (*command_line - 'A' + 10);
+          }
+          command_line++;
+      }
     
-    Status = ParseCommandLine(command_line, &Addr, &EndAddr, &Stalltime);
-    if (EFI_ERROR(Status)) {
-        Print(L"Failed to parse command line: %r\n", Status);
-        return Status;
-    }
+      while (*command_line == ' ') command_line++;  // Skip spaces
+    
+      // Parse the second address (Hex)
+      EndAddr = 0;
+      while (*command_line && *command_line != ' ') {
+          if (*command_line >= '0' && *command_line <= '9') {
+              EndAddr = (EndAddr << 4) | (*command_line - '0');
+          } else if (*command_line >= 'a' && *command_line <= 'f') {
+              EndAddr = (EndAddr << 4) | (*command_line - 'a' + 10);
+          } else if (*command_line >= 'A' && *command_line <= 'F') {
+              EndAddr = (EndAddr << 4) | (*command_line - 'A' + 10);
+          }
+          command_line++;
+      }
 
-    Print(L"Parsed values - Addr: 0x%lx, EndAddr: 0x%lx, Stalltime: %lu ms\n", Addr, EndAddr, Stalltime);
+      initialAddressRange = AllocatePool(sizeof(struct ADDRESS_RANGE));
+      initialAddressRange->next = 0x0;
 
+      StartAddr /= EFI_PAGE_SIZE;
+      EndAddr /= EFI_PAGE_SIZE;
+      initialAddressRange->pageCount = EndAddr - StartAddr + 1;
+      StartAddr *= EFI_PAGE_SIZE;
+
+      initialAddressRange->startAddress = StartAddr;
+    
+      while (*command_line == ' ') command_line++;  // Skip spaces
+    
+      // Parse the stall time (Decimal)
+      *Stalltime = 0;
+      while (*command_line && *command_line >= '0' && *command_line <= '9') {
+          *Stalltime = (*Stalltime * 10) + (*command_line - '0');
+          command_line++;
+      }
+      break;
+  }
+
+
+  return initialAddressRange;
+}
+
+
+EFI_STATUS efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
+  EFI_STATUS Status;
+  EFI_LOADED_IMAGE *loaded_image;
+  UINTN Stalltime;
+  struct ADDRESS_RANGE *addressRanges;
+
+  Status = uefi_call_wrapper(BS->HandleProtocol, 3, ImageHandle, &LoadedImageProtocol, (void **)&loaded_image);
+  if (EFI_ERROR(Status)) {
+     Print(L"Error HandleProtocol: %r\n", Status);
+     uefi_call_wrapper(BS->Stall, 1, 5000000);
+     return Status;
+  }
+
+  CHAR16 *command_line = (CHAR16 *)loaded_image->LoadOptions;
+  if (command_line == NULL) {
+      Print(L"No command line arguments provided.\n");
+      return EFI_INVALID_PARAMETER;
+  }
+
+  addressRanges = ParseCommandLine(command_line, &Stalltime);
+
+  if (addressRanges == 0x0) {
+      Print(L"Failed to parse command line.\n");
+      return EFI_INVALID_PARAMETER;
+  }
+  
+  while (addressRanges != 0x0)
+  {
+
+    Print(L"Parsed values - Addr: 0x%lx, pageNum: %lu, Stalltime: %lu ms\n", addressRanges->startAddress, addressRanges->pageCount, Stalltime);
+  
     Print(L"Disable RAM Area...!\n");
-
-    // Align the starting address (Addr) to the previous page boundary
-    Addr = Addr & ~(EFI_PAGE_SIZE - 1);
-
-    // Calculate the number of pages between the aligned Addr and EndAddr
-    UINTN NumPages = ((EndAddr - Addr) + EFI_PAGE_SIZE - 1) / EFI_PAGE_SIZE;
-
-    Print(L"Disable from 0x%lx to 0x%lx, which is %lu pages\n\n", Addr, EndAddr, NumPages);
-
-    Status = uefi_call_wrapper(BS->AllocatePages, 4, 2, 8, NumPages, &Addr);
-
+  
+    Status = uefi_call_wrapper(BS->AllocatePages, 4, 2, 8, addressRanges->pageCount, &(addressRanges->startAddress));
+  
     if (EFI_ERROR(Status)) {
         Print(L"Error!\n");
         Print(L"AllocatePages failed: %r\n", Status);
@@ -114,11 +178,17 @@ efi_main (EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
         return Status;  // Return the error status
     } else {
         Print(L"Success!\n");
-        Print(L"Memory allocated at address: 0x%lx for %lu pages\n", Addr, NumPages);
+        Print(L"Memory allocated at address: 0x%lx for %lu pages\n", addressRanges->startAddress, addressRanges->pageCount);
     }
 
-    uefi_call_wrapper(BS->Stall, 1, Stalltime * 1000);
+    Print(L"Checking for more address ranges...\n");
 
-    Exit(EFI_SUCCESS, 0,0);
-    return EFI_SUCCESS;
+    addressRanges = addressRanges->next;
+    //addressRanges should be null when it runs out of stuff.
+  }
+
+  uefi_call_wrapper(BS->Stall, 1, Stalltime * 1000);
+
+  Exit(EFI_SUCCESS, 0,0);
+  return EFI_SUCCESS;
 }
